0d335bb5152efb2bae6af2364bb19266ff77ee32
min01 <goyoumin2@gmail.com>
2026-07-31 16:11:10 +0900

    removed reflection

--- src/main/java/com/min01/solomonlib/util/SolomonUtil.java
+++ src/main/java/com/min01/solomonlib/util/SolomonUtil.java
@@ -1,20 +1,15 @@
 package com.min01.solomonlib.util;
 
-import java.lang.reflect.Method;
 import java.util.UUID;
 
 import io.netty.buffer.ByteBuf;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.entity.LevelEntityGetter;
 import net.minecraft.world.phys.Vec3;
-import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
 
 public class SolomonUtil
 {
-	public static final Method GET_ENTITY = ObfuscationReflectionHelper.findMethod(Level.class, "m_142646_");
-	
 	public static ByteBuf writeVec3(FriendlyByteBuf buf, Vec3 vec3)
 	{
 		buf.writeDouble(vec3.x);
@@ -23,38 +18,19 @@ public class SolomonUtil
 		return buf;
 	}
 	
-	public static Vec3 readVec3(ByteBuf buf)
+	public static Vec3 readVec3(FriendlyByteBuf buf)
 	{
 		return new Vec3(buf.readDouble(), buf.readDouble(), buf.readDouble());
 	}
 	
-	@SuppressWarnings("unchecked")
 	public static Iterable<Entity> getAllEntities(Level level)
 	{
-		try 
-		{
-			LevelEntityGetter<Entity> entities = (LevelEntityGetter<Entity>) GET_ENTITY.invoke(level);
-			return entities.getAll();
-		}
-		catch (Exception e) 
-		{
-			e.printStackTrace();
-		}
-		return null;
+		return level.getEntities().getAll();
 	}
 	
 	@SuppressWarnings("unchecked")
 	public static <T extends Entity> T getEntityByUUID(Level level, UUID uuid)
 	{
-		try 
-		{
-			LevelEntityGetter<Entity> entities = (LevelEntityGetter<Entity>) GET_ENTITY.invoke(level);
-			return (T) entities.get(uuid);
-		}
-		catch (Exception e) 
-		{
-			e.printStackTrace();
-		}
-		return null;
+		return (T) level.getEntities().get(uuid);
 	}
 }
